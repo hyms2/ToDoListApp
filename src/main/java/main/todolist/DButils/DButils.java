68f8db8e3163a33a719613ad869ce8113e186b71
Hazim <68647806+hyms2@users.noreply.github.com>
2024-04-30 23:13:41 +0700

    Update DButils.java

--- src/main/java/main/todolist/DButils/DButils.java
+++ src/main/java/main/todolist/DButils/DButils.java
@@ -19,8 +19,8 @@ import java.util.ArrayList;
 
 public class DButils {
     private static final String URL = "jdbc:mysql://localhost:3306/todolistdb";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "hazim1234";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "passoword";
 
 
     private static Connection getConnection() throws SQLException {
